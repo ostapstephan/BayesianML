0b6cb4bb23f092ed49cf5f74ae4ef32674fc6295
ostapstephan <ostapstephan@gmail.com>
2019-12-22 18:12:09 -0500

    Project 5 submitted

--- project5/project5.py
+++ project5/project5.py
@@ -28,6 +28,9 @@ For 2-D, create a plot similar to 9.8, but with K = 3. If you want to get fancy,
 
 def confidence_ellipse(mean,cov, ax, n_std=3.0, facecolor='none', **kwargs):
     """
+    This function is an adapted version of the one found here 
+    https://matplotlib.org/3.1.0/gallery/statistics/confidence_ellipse.html
+            
     Create a plot of the covariance confidence ellipse of `x` and `y`
 
     Parameters
@@ -89,7 +92,6 @@ class EM:
         gamma = np.zeros([mu.shape[0],x.shape[0]])  
         d=[]
         for k in range(mu.shape[0]):
-            # vec =  mv_norm(x,mu[k],sig[k])
             vec = norm.pdf(x,mu[k],sigma[k])
             d.append( pie[k]*vec )
         denominator=sum(d)
@@ -114,13 +116,13 @@ class EM:
             sum_gamma_and_mu = np.array(sum_gamma_and_mu)
             sigma[k] = np.sqrt( 1/N[k] * np.sum(sum_gamma_and_mu,0) )
         pie = N / np.sum(N)
-
         return mu, sigma, pie
 
     def e_2d(self,x,mu,sigma,pie):       
         gamma = np.zeros([mu.shape[0],x.shape[0]])  
         d=[]
-        for k in range(mu.shape[0]):#3 num clusters
+        # calculate the responsibility params for each of the 3 clusters
+        for k in range(mu.shape[0]):
             vec = multivariate_normal(mu[k],sigma[k])
             d.append( pie[k]*vec.pdf(x) ) #scalar*scalar
         denominator=sum(d)
@@ -130,8 +132,8 @@ class EM:
         return gamma.T
 
     def m_2d(self,x,gamma):
-        N = np.sum(gamma, 0) # N should be shape [k]
-        # Gamma is  [n,k] we should have k mu's
+        N = np.sum(gamma, 0) # N should be the shape of the number of clusters 
+        # Gamma is  [n,k] and so we should have k mu's
         mu=[[] for x in range(gamma.shape[1])]
 
         # calculate mu for each cluster
@@ -141,10 +143,10 @@ class EM:
         mu = np.array(mu)
         sigma = np.ones([3,2,2])
 
-        # calculate Cov and Pi
+        # calculate Cov mat and Pi
         for k in range(mu.shape[0]):
             sum_gamma_and_mu=[]
-            for n in range(x.shape[0]):#300
+            for n in range(x.shape[0]):
                 a = np.expand_dims((x[n]-mu[k]), 1)
                 sum_gamma_and_mu.append( gamma[n,k] * a@a.T )
             
@@ -162,25 +164,25 @@ class EM:
             mu  = np.array([np.random.uniform(meanRange[0],meanRange[1],(self.n_dims)) for j  in range(k) ]).flatten()
             sig = np.array([np.random.uniform(0.1,meanRange[1],[self.n_dims for x in range(self.n_dims)]) for j in range(k)]).flatten()
 
-            pie = np.array([len(x)/(len(x)*k) for j in range(k)]).flatten() # its probably safe to assume there are equal number of samples from each sourcek
+            pie = np.array([len(x)/(len(x)*k) for j in range(k)]).flatten() 
+            # its probably safe to assume there are equal number of samples from each source
             mu_,sig_,pie_= [],[],[]
 
+            # put down your estimate for the random guess with no training 
             if n_iter[0] == 0:
-                # put down your est for the initial guess with no training 
                 mu_,sig_,pie_= [mu],[sig],[pie]
 
             for i in range(n_iter[-1]):
                 gamma = self.e(x,mu,sig,pie) 
-                # mu, sig, pie = self.m1(gamma, x, mu, sig, pie )
                 mu, sig, pie = self.m(x,gamma) 
                 if i+1 in n_iter:
                     mu_.append(mu)
                     sig_.append(sig)
                     pie_.append(pie)
         else:
-            mu  = np.array([np.random.uniform(meanRange[0],meanRange[1],(self.n_dims)) for j  in range(k) ])#.flatten()
+            mu  = np.array([np.random.uniform(meanRange[0],meanRange[1],(self.n_dims)) for j  in range(k) ])
             size = [self.n_dims for x in range(self.n_dims)]
-            s = np.array([np.random.uniform(0.1,meanRange[1],size) for j in range(k)])#.flatten()
+            s = np.array([np.random.uniform(0.1,meanRange[1],size) for j in range(k)])
             sig = [ x @ x.T for x in s ]
 
             pie = np.array([len(x)/(len(x)*k) for j in range(k)]).flatten() 
@@ -188,13 +190,12 @@ class EM:
 
             mu_,sig_,pie_= [],[],[]
 
+            # put down your est for the initial guess with no training 
             if n_iter[0] == 0:
-                # put down your est for the initial guess with no training 
                 mu_,sig_,pie_= [mu],[sig],[pie]
             
             for i in range(n_iter[-1]):
                 gamma = self.e_2d(x,mu,sig,pie) 
-                # mu, sig, pie = self.m1(gamma, x, mu, sig, pie )
                 mu, sig, pie = self.m_2d(x,gamma) 
                 if i+1 in n_iter:
                     mu_.append(mu)
@@ -225,8 +226,6 @@ class EM:
             pie = np.array([n0,n1,n2])/self.N
 
         elif ndims ==2:
-
-            # ndims > 1 case 
             mu0 = np.random.uniform(meanRange[0],meanRange[1],(ndims))
             mu1 = np.random.uniform(meanRange[0],meanRange[1],(ndims))
             mu2 = np.random.uniform(meanRange[0],meanRange[1],(ndims))
@@ -235,7 +234,7 @@ class EM:
             sig1 = np.random.uniform(meanRange[0]/3,meanRange[1]/3,[ndims for x in range(ndims)])
             sig2 = np.random.uniform(meanRange[0]/3,meanRange[1]/3,[ndims for x in range(ndims)]) 
 
-            # I need to make sure the cov is semidefinite or the thing will complain
+            # I need to make sure the cov is positive semidefinite   
             sig0 = sig0@sig0.T
             sig1 = sig1@sig1.T
             sig2 = sig2@sig2.T
@@ -248,29 +247,10 @@ class EM:
             data_1 = np.random.multivariate_normal(mu1,sig1,(n1))
             data_2 = np.random.multivariate_normal(mu2,sig2,(n2))
 
-
             x   = np.concatenate([data_0,data_1,data_2])
             mu  = np.array([mu0,mu1,mu2])
             sig = np.array([sig0,sig1,sig2])
             pie = np.array([n0,n1,n2])/(np.sum([n0,n1,n2]))
-
-
-            '''
-            plt.rcParams['figure.figsize'] = (16,9) # make the plot bigger
-            plt.gca().set_aspect('equal', adjustable='box')
-            ax =plt.gca()
-            plt.xlim(-20, 20)
-            plt.ylim(-15, 15)
-
-            for c,data in enumerate([data_0,data_1,data_2]):
-                x = data[:,0]
-                y = data[:,1]
-                plt.scatter(x,y,linewidths=2)
-                confidence_ellipse(mu[c], sig[c], ax, n_std=1, edgecolor='orange')
-
-            plt.show()
-            plt.clf
-            '''
 
         else:
             raise ValueError(f'Number of dimentions must be greater than or equal to 1, you passed: {ndims}')
@@ -278,7 +258,7 @@ class EM:
     
 def One_D():
     seed = 8
-    N = 30000 # num data points per cat
+    N = 30000 # num data points 
     ndims=1
     em_alg= EM( N,seed ,ndims)
     data, mu, sig, pie =  em_alg.gen_data((-10,10), ndims=ndims)
@@ -321,7 +301,7 @@ def One_D():
        
 def Two_D():
     seed = 24
-    N = 3000  # num data points per cat
+    N = 3000  # num data points 
     ndims = 2
     em_alg = EM( N, seed ,ndims )
     data, mu, sig, pie =  em_alg.gen_data((-10,10), ndims=ndims)
@@ -336,7 +316,6 @@ def Two_D():
     mu_, sig_, pie_ = em_alg.train(data,k,N_train_steps)
 
     print('stuff',mu_.shape, sig_.shape)
-    # stuff (4, 3, 2) (4, 3) sig is 1x1 but needs to be 2x2 so (4,3,2,2)
     plt.rcParams['figure.figsize'] = (16,9) # make the plot bigger
     for loc in range(4) :
                 
@@ -348,7 +327,7 @@ def Two_D():
 
         # PLOT THE TRUE ELLIPSES
         for c in range(3):
-            # Plot the true ellipse colors 
+            # Plot the true ellipse 
             confidence_ellipse(mu[c], sig[c], ax, n_std=1, edgecolor='blue')
 
         # Draw the predicted confidence ellipses   
@@ -359,7 +338,6 @@ def Two_D():
         plt.gca().set_aspect('equal', adjustable='box')
         ax = plt.gca()
         plt.xlim(-25, 25)
-        
         plt.ylim(-15.5, 15.5)
 
         print('Number of training steps:', N_train_steps[loc])
@@ -369,7 +347,6 @@ def Two_D():
         
     plt.matplotlib.pyplot.savefig(f'plots/GMM_2D_Case.png',dpi=800)
     plt.show()
-
     plt.clf()
 
 
