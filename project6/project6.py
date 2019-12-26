210f64266a5ab4c68b13cc1a4718cdba895c6f02
ostapstephan <ostapstephan@gmail.com>
2019-12-26 02:05:27 -0500

    Done w hw6

--- project6/project6.py
+++ project6/project6.py
@@ -1,7 +1,10 @@
 # python 3
-# Ostap Voynarovskiy 
+# Ostap Voynarovskiy  
 # Prof. Keene
-# Project 5
+# Project 6
+
+# Isaac Alboucai helped me figure out the metropolis hastings 
+# part of the assignment so I am citing him
 
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -13,7 +16,6 @@ from scipy.stats import multivariate_normal
 from scipy.stats import norm 
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-
 
 
 
@@ -85,8 +87,7 @@ def part1():
     plt.savefig('plots/Rejection_Sampling_Envelope.png')
     
     plt.show()
-
-
+    plt.clf()
 
 
 
@@ -103,8 +104,8 @@ samples
 
 Use Equation 3.10 as the likelihood function, to be used with the training samples 
 you generated. You may select any distribution you want for the prior on the weights, 
-and recall that the posterior density on the weights w is proportional to the likelihood 
-x prior
+and recall that the posterior density on the weights w is proportional to the 
+likelihood * prior
 
 Use the Metropolis algorithm as defined in equation 11.33 to compute an estimate 
 of the weights.
@@ -112,7 +113,6 @@ of the weights.
 A few practical tips - you'll need to use the log of the posterior, i.e.  log (likelihood 
 x prior) instead of the actual probabilities, due to numerical precision problems 
 that will crop up with 25 training observations.
-
 
 Remember to give the Markov chain a chance to 'burn in' and let it run for a few 
 hundred samples or so before you start using those samples to compute an average 
@@ -127,28 +127,84 @@ the proposal distribution on the previous stored sample.  See the bottom of page
 
 '''
 
-# Linear regression 
+
+def loglikelyhood(tn,w,phi,beta):
+    # Function to calculate the loglikelyhood for use in the metropolis-hastings algorithm 
+    return np.sum(np.log(norm.pdf(tn,(w@phi.T).T,np.sqrt(1/beta))))
+
+def Metropolis_Hastings( burn_in, total_samples, t, phi, beta, init):
+    #Function to run the Metropolis-Hastings algorithm
+    prop_mean = np.array([0,0])
+
+    #prior distribution mean and covaraiance
+    prior_mean = np.array([0,0])
+    prior_cov = np.eye(2)
+    prop_cov = np.eye(2)*.2
+    curr_weights = prop_mean
+
+    count = 0
+    weights = []
+
+    # initialize var
+    numer = 0
+    denom = 0
+
+
+    #  the metropolis-hastings algorithm
+    while(len(weights)< total_samples):
+        if count%100==10:
+            print(count)
+            print(prob)
+
+        prev_weights = curr_weights
+        count+=1
+        
+        curr_weights = np.random.multivariate_normal(mean = prop_mean, cov = prop_cov)
+
+        if init:
+            # On the first run the likelyhood plotting the  
+            numer = 0
+            denom = 0
+        else:
+            numer = loglikelyhood(t,np.array(curr_weights),phi,beta) 
+            denom = loglikelyhood(t,np.array(prev_weights),phi,beta)
+
+        numer+= np.log(multivariate_normal.pdf(curr_weights,mean=prior_mean,cov=prior_cov)) \
+                +np.log(multivariate_normal.pdf(curr_weights,mean=prev_weights,cov=prop_cov))
+
+        denom+= np.log(multivariate_normal.pdf(prev_weights,mean=prior_mean,cov=prior_cov))\
+                + np.log(multivariate_normal.pdf(prev_weights,mean=curr_weights,cov=prop_cov))
+
+        # logs make division into subtraction 
+        prob = numer-denom 
+
+        if(prob>=0):  
+            # if p>1 keep it 
+            if(count > burn_in): 
+                #only keep if we're past the min burn in amount
+                weights.append(curr_weights)
+        else: 
+            # if p<1 keep w probability p 
+            if(prob > np.log(np.random.rand(1)) ):
+                if(count > burn_in):
+                    weights.append(curr_weights)
+            else:
+                count-=1
+                curr_weights=prev_weights
+    return weights, np.mean(weights,axis=0)
+
 
 def GenData(x, A0,A1, sigma):
     # gen data along the line A0 + x*A1 and add mean 0 std sigma noise on top
     rand_noise =  np.random.normal(0, sigma,size=(len(x)) )
     Target = A0 + x*(A1) + rand_noise 
     return Target
-
+    
 def likelihood(x, t, w, beta):
-    # calculate the likelihood
+    # calculate the likelihood for the single point
     iota = np.concatenate((np.ones((len(x),1)), x*np.ones((len(x),1))), axis = 1)
     p = scipy.stats.norm(w@iota.T, beta**(-1))    
     return np.squeeze(p.pdf(t))
-
-def posterior(w,x,t,alpha,beta):
-    # iota = np.expand_dims(np.concatenate((np.ones(x.shape[0]), x),axis=1 ),0)
-    iota = np.column_stack((np.ones(x.shape[0]), x))
-
-    sn = np.linalg.inv(alpha*np.eye(iota.shape[1]) + beta*iota.T@iota)
-    mn = (beta* (sn@iota.T)@t)
-    posterior = scipy.stats.multivariate_normal(np.squeeze(mn), sn) 
-    return posterior.pdf(w), posterior
 
 def part2():
     # Generate Data to model (linear model)
@@ -173,34 +229,37 @@ def part2():
     # Plot the Prior dist. (a multivariate gaussian)
     loc = 2  
     plt.subplot(4,3,loc) 
-
-    # generate the meshgrid to plot contour of the prior
-    x, y = np.mgrid[-1:1:.001, -1:1:.001]
-    grid = np.empty(x.shape + (2,)) 
     
-    # plot prior 
-    grid[:, :, 0] = x
-    grid[:, :, 1] = y 
-    rv = scipy.stats.multivariate_normal([0,0],(alpha**(-1))*np.eye(2)) 
-    z = rv.pdf(grid)
-    plt.contourf(x, y, z, levels=50, cmap = 'jet')
+    burn_in=1000
+    total_samples= 10000
+    phi = None # this will be ignored since this is the prior 
+    init = True
+    w,w_avg = Metropolis_Hastings(burn_in,total_samples,tn[0:1],phi, beta,init )
+    w = np.array(w) 
+
+    x,y= w[:,0],w[:,1]
     plt.plot(a0,a1,'+')
+
+    plt.hist2d(x, y, bins=100, range=[[-1,1],[-1,1]] ,cmap = 'jet')
+    plt.xlabel('x')
+    plt.ylabel('y')
 
     plt.xlabel('W0', fontsize=10)
     plt.ylabel('W1',labelpad=30, fontsize=10, rotation=0)
     plt.title('Prior/Posterior')
     plt.axis('square')
+
     
     # Plot the dataspace of the prior
     # (sample the weights plot the equations)
     loc = 3
     plt.subplot(4,3,loc) 
-    w = rv.rvs(6)
+    ws =  w[np.random.randint(0,len(w),7),:]
     x1 = np.linspace(-1,1,num=100)
     
-    for i in range(len(w)):
-        #loop 6 times and print the samples of the w mat as lines
-        yTemp = w[i,0] + x1*w[i,1]
+    for i in range(len(ws)):
+        #loop 7 times and print the samples of the w mat as lines
+        yTemp = ws[i,0] + x1*ws[i,1]
         plt.plot(x1,yTemp)
 
 
@@ -211,7 +270,7 @@ def part2():
     plt.ylabel('Y',labelpad=10, fontsize=10, rotation=0)
     plt.title('Data Space')
     
-    # Plot the Likelihood
+    # Plot the Likelihood 
     loc = 4
     plt.subplot(4,3,loc) 
      
@@ -232,14 +291,20 @@ def part2():
     loc = 5
     plt.subplot(4,3,loc) 
 
-    w0, w1 = np.mgrid[-1:1:.01, -1:1:.01]
-    grid = np.empty(w0.shape + (2,)) 
-    grid[:, :, 0] = w0
-    grid[:, :, 1] = w1
+    # one data point
+    burn_in=1000
+    total_samples= 10000
+    phi = np.column_stack((np.ones(x_data[0:1].shape[0]), x_data[0:1]))
+    init = False 
+    w,w_avg = Metropolis_Hastings(burn_in,total_samples,tn[0:1],phi, beta,init )
+    w = np.array(w) 
 
-    z, post = posterior(grid, x_data[0:1], tn[0:1], alpha, sigma**-1)
-    plt.contourf(w0, w1, z , levels=50, cmap = 'jet')
+    x,y= w[:,0],w[:,1]
     plt.plot(a0,a1,'+')
+
+    plt.hist2d(x, y, bins=100, range=[[-1,1],[-1,1]] ,cmap = 'jet')
+    plt.xlabel('x')
+    plt.ylabel('y')
 
     plt.xlabel('W0', fontsize=10)
     plt.ylabel('W1',labelpad=30, fontsize=10, rotation=0)
@@ -248,12 +313,13 @@ def part2():
 
     loc = 6
     plt.subplot(4,3,loc) 
-    w = post.rvs(6)
+    # plot Dataspace
+    ws =  w[np.random.randint(0,len(w),7),:]
     x1 = np.linspace(-1,1,num=100)
-     
-
-    for i in range(len(w)):
-        yTemp = w[i,0] + x1*w[i,1]
+    
+    for i in range(len(ws)):
+        #loop 7 times and print the samples of the w mat as lines
+        yTemp = ws[i,0] + x1*ws[i,1]
         plt.plot(x1,yTemp)
 
     plt.plot( x_data[0], tn[0], 'bo')
@@ -264,7 +330,7 @@ def part2():
     plt.xlabel('X', fontsize=10)
     plt.ylabel('Y',labelpad=10, fontsize=10, rotation=0)
     plt.title('Data Space')
-  
+
     # Plot the Likelihood
     loc = 7
     plt.subplot(4,3,loc) 
@@ -286,14 +352,20 @@ def part2():
     loc = 8
     plt.subplot(4,3,loc) 
     
-    w0, w1 = np.mgrid[-1:1:.01, -1:1:.01]
-    grid = np.empty(w0.shape + (2,)) 
-    grid[:, :, 0] = w0
-    grid[:, :, 1] = w1
+    # two data points
+    burn_in=1000
+    total_samples= 10000
+    phi = np.column_stack((np.ones(x_data[0:2].shape[0]), x_data[0:2]))
+    init = False 
+    w,w_avg = Metropolis_Hastings(burn_in,total_samples,tn[0:2],phi, beta,init )
+    w = np.array(w) 
 
-    z, post = posterior(grid, x_data[0:2], tn[0:2], alpha, sigma**-1)
-    plt.contourf(w0, w1, z , levels=50, cmap = 'jet')
+    x,y= w[:,0],w[:,1]
     plt.plot(a0,a1,'+')
+
+    plt.hist2d(x, y, bins=100, range=[[-1,1],[-1,1]] ,cmap = 'jet')
+    plt.xlabel('x')
+    plt.ylabel('y')
 
     plt.xlabel('W0', fontsize=10)
     plt.ylabel('W1',labelpad=30, fontsize=10, rotation=0)
@@ -302,14 +374,16 @@ def part2():
 
     loc = 9
     plt.subplot(4,3,loc) 
-    w = post.rvs(6)
+    ws =  w[np.random.randint(0,len(w),7),:]
     x1 = np.linspace(-1,1,num=100)
-     
-    for i in range(len(w)):
-        yTemp = w[i,0] + x1*w[i,1]
+    
+    for i in range(len(ws)):
+        #loop 7 times and print the samples of the w mat as lines
+        yTemp = ws[i,0] + x1*ws[i,1]
         plt.plot(x1,yTemp)
 
-    plt.plot( x_data[0:2], tn[0:2] ,'bo')
+    plt.plot( x_data[0:2], tn[0:2], 'bo')
+
     plt.axis('square')
     plt.ylim(bottom=-1,top= 1) 
     plt.xlim(left =-1,right=1)
@@ -317,6 +391,7 @@ def part2():
     plt.ylabel('Y',labelpad=10, fontsize=10, rotation=0)
     plt.title('Data Space')
     
+
     # Plot the Likelihood
     loc = 10
     plt.subplot(4,3,loc) 
@@ -337,15 +412,20 @@ def part2():
     
     loc = 11
     plt.subplot(4,3,loc) 
-    
-    w0, w1 = np.mgrid[-1:1:.01, -1:1:.01]
-    grid = np.empty(w0.shape + (2,)) 
-    grid[:, :, 0] = w0
-    grid[:, :, 1] = w1
+    # all training data points
+    burn_in=1000
+    total_samples= 10000
+    phi = np.column_stack((np.ones(x_data[:].shape[0]), x_data[:]))
+    init = False 
+    w,w_avg = Metropolis_Hastings(burn_in,total_samples,tn[:],phi, beta,init )
+    w = np.array(w) 
 
-    z, post = posterior(grid, x_data[0:], tn[0:], alpha, sigma**-1)
-    plt.contourf(w0, w1, z , levels=50, cmap = 'jet')
+    x,y= w[:,0],w[:,1]
     plt.plot(a0,a1,'+')
+
+    plt.hist2d(x, y, bins=100, range=[[-1,1],[-1,1]] ,cmap = 'jet')
+    plt.xlabel('x')
+    plt.ylabel('y')
 
     plt.xlabel('W0', fontsize=10)
     plt.ylabel('W1',labelpad=30, fontsize=10, rotation=0)
@@ -354,14 +434,15 @@ def part2():
 
     loc = 12
     plt.subplot(4,3,loc) 
-    w = post.rvs(6)
+    ws =  w[np.random.randint(0,len(w),7),:]
     x1 = np.linspace(-1,1,num=100)
-     
-    for i in range(len(w)):
-        yTemp = w[i,0] + x1*w[i,1]
-        plt.plot(x1,yTemp)
     
-    plt.plot( x_data[0:], tn[0:],'bo')
+    for i in range(len(ws)):
+        #loop 7 times and print the samples of the w mat as lines
+        yTemp = ws[i,0] + x1*ws[i,1]
+        plt.plot(x1,yTemp)
+
+    plt.plot( x_data[:], tn[:], 'bo')
 
     plt.axis('square')
     plt.ylim(bottom=-1,top= 1) 
@@ -369,9 +450,9 @@ def part2():
     plt.xlabel('X', fontsize=10)
     plt.ylabel('Y',labelpad=10, fontsize=10, rotation=0)
     plt.title('Data Space')
-    
+
     plt.tight_layout()
-    plt.savefig('project_2_graphs/3_7.pdf')
+    plt.savefig('plots/3_7.pdf')
 
 
 
@@ -380,6 +461,6 @@ if __name__ == '__main__':
     ########## Rejection Sampling ###########
     part1()
 
-    ########## 2D 3 Cluster Case ###########
-    # part2()
+    ########## MCMC linear Regression  ###########
+    part2()
     
